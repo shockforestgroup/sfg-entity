668da995aed486e7e4bedfd3e8774c2e1ee5e217
alexabruck <5918060+alexabruck@users.noreply.github.com>
2020-08-17 17:05:03 +0200

    Rewrite as React class

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import { render } from "react-dom";
 import { Stage, Layer, Text, Circle, Line } from "react-konva";
 
-const answers = [{ text: "Yes" }, { text: "No" }, { text: "Maybe" }];
+import Answer from "./components/Answer";
+
+const ANSWERS = [
+  { id: "yes", text: "Yes" },
+  { id: "no", text: "No" },
+  { id: "maybe", text: "Maybe" },
+];
+
+function haveIntersection(r1, r2) {
+  return !(
+    r2.x > r1.x + r1.width ||
+    r2.x + r2.width < r1.x ||
+    r2.y > r1.y + r1.height ||
+    r2.y + r2.height < r1.y
+  );
+}
 
 function generateShapes() {
   return [...Array(10)].map((_, i) => ({
@@ -14,116 +29,161 @@ function generateShapes() {
   }));
 }
 
-const INITIAL_STATE = { circles: generateShapes(), traceLines: {} };
+const INITIAL_STATE = { circles: generateShapes() };
 
-const App = () => {
-  const [circles, setCircles] = React.useState(INITIAL_STATE.circles);
-  const [traceLines, setTraceLines] = React.useState(INITIAL_STATE.traceLines);
-  const [cursorType, setCursorType] = React.useState("default");
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      circles: INITIAL_STATE.circles,
+      traceLines: {},
+      cursorType: "default",
+      isTouching: "yes",
+      isLanded: "no",
+    };
+  }
 
-  const updateDragLine = (id, point) => {
+  answerRefs = {};
+
+  handleDragHover = (e) => {
+    console.log(this.answerRefs);
+    Object.keys(this.answerRefs).forEach((id) => {
+      const ref = this.answerRefs[id];
+      if (haveIntersection(e.target.getClientRect(), ref.getClientRect())) {
+        this.setState({ isTouching: id });
+      } else {
+        this.setState({ isTouching: null });
+      }
+    });
+  };
+
+  handleDropLanding = (e) => {
+    console.log(this.answerRefs);
+    Object.keys(this.answerRefs).forEach((id) => {
+      const ref = this.answerRefs[id];
+      if (haveIntersection(e.target.getClientRect(), ref.getClientRect())) {
+        this.setState({ isLanded: id });
+      }
+    });
+  };
+
+  updateDragLine = (id, point) => {
     /* append to existing trace line points for this element */
     const elementTraceLinePoints = [
-      ...(traceLines[id] || []),
+      ...(this.state.traceLines[id] || []),
       point.x,
       point.y,
     ];
-    setTraceLines({ ...traceLines, [id]: elementTraceLinePoints });
-    //console.log("event", e.evt.clientX, e.evt.clientY);
+    this.setState({
+      traceLines: { ...this.state.traceLines, [id]: elementTraceLinePoints },
+    });
   };
 
-  const handleDragStart = (e) => {
+  handleDragStart = (e) => {
     const id = e.target.id();
-    setCircles(
-      circles.map((circle) => {
+    this.setState({
+      circles: this.state.circles.map((circle) => {
         return {
           ...circle,
           isDragging: circle.id === id,
         };
-      })
-    );
-    updateDragLine(e.target.id(), {
+      }),
+    });
+
+    this.updateDragLine(e.target.id(), {
       x: e.target.attrs.x,
       y: e.target.attrs.y,
     });
-    setCursorType("grabbing");
+    this.setState({ cursorType: "grabbing" });
   };
 
-  const handleDragEnd = (e) => {
-    setCircles(
-      circles.map((circle) => {
+  handleDragMove = (e) => {
+    this.handleDragHover(e);
+    this.updateDragLine(e.target.id(), {
+      x: e.target.attrs.x,
+      y: e.target.attrs.y,
+    });
+  };
+
+  handleDragEnd = (e) => {
+    this.handleDropLanding(e);
+    this.setState({
+      circles: this.state.circles.map((circle) => {
         return {
           ...circle,
           isDragging: false,
         };
-      })
+      }),
+    });
+    this.updateDragLine(e.target.id(), {
+      x: e.target.attrs.x,
+      y: e.target.attrs.y,
+    });
+    this.setState({ cursorType: "grab" });
+  };
+
+  render() {
+    return (
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        style={{ cursor: this.state.cursorType }}
+      >
+        <Layer>
+          {ANSWERS.map(({ id, text }, i) => (
+            <Answer
+              id={id}
+              ref={(ref) => {
+                console.log("set ref");
+                this.answerRefs[id] = ref;
+              }}
+              text={text}
+              x={(window.innerWidth / 4) * (i + 1)}
+              y={20}
+              isTriggered={this.state.isTouching === id}
+              isActivated={this.state.isLanded === id}
+            />
+          ))}
+
+          {Object.keys(this.state.traceLines).map((key) => (
+            <Line
+              key={key}
+              id={key}
+              points={this.state.traceLines[key]}
+              stroke="blue"
+              strokeWidth={2}
+            />
+          ))}
+
+          {this.state.circles.map((circle) => (
+            <Circle
+              key={circle.id}
+              id={circle.id}
+              x={circle.x}
+              y={circle.y}
+              stroke="black"
+              fill="#fff"
+              radius={10}
+              rotation={circle.rotation}
+              scaleX={circle.isDragging ? 1.05 : 1}
+              scaleY={circle.isDragging ? 1.05 : 1}
+              draggable
+              /* dragBoundFunc={() => {}} */
+              onDragStart={this.handleDragStart}
+              onDragMove={this.handleDragMove}
+              onDragEnd={this.handleDragEnd}
+              onMouseEnter={() => {
+                this.setState({ cursorType: "grab" });
+              }}
+              onMouseLeave={() => {
+                this.setState({ cursorType: "default" });
+              }}
+            />
+          ))}
+        </Layer>
+      </Stage>
     );
-    updateDragLine(e.target.id(), {
-      x: e.target.attrs.x,
-      y: e.target.attrs.y,
-    });
-    setCursorType("grab");
-  };
-
-  const handleDragMove = (e) => {
-    const id = e.target.id();
-    updateDragLine(id, {
-      x: e.target.attrs.x,
-      y: e.target.attrs.y,
-    });
-  };
-
-  const clearTraces = () => setTraceLines({});
-
-  return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      style={{ cursor: cursorType }}
-    >
-      <Layer>
-        <Text text="Clear traces" onClick={clearTraces} />
-
-        <Text text={answers[0].text} x={window.innerWidth / 4} />
-        <Text text={answers[1].text} x={(window.innerWidth / 4) * 2} />
-        <Text text={answers[2].text} x={(window.innerWidth / 4) * 3} />
-        {Object.keys(traceLines).map((key) => (
-          <Line
-            key={key}
-            id={key}
-            points={traceLines[key]}
-            stroke="blue"
-            strokeWidth={2}
-          />
-        ))}
-
-        {circles.map((circle) => (
-          <Circle
-            key={circle.id}
-            id={circle.id}
-            x={circle.x}
-            y={circle.y}
-            stroke="black"
-            fill="#fff"
-            radius={10}
-            rotation={circle.rotation}
-            scaleX={circle.isDragging ? 1.05 : 1}
-            scaleY={circle.isDragging ? 1.05 : 1}
-            draggable
-            onDragStart={handleDragStart}
-            onDragMove={handleDragMove}
-            onDragEnd={handleDragEnd}
-            onMouseEnter={() => {
-              setCursorType("grab");
-            }}
-            onMouseLeave={() => {
-              setCursorType("default");
-            }}
-          />
-        ))}
-      </Layer>
-    </Stage>
-  );
-};
+  }
+}
 
 render(<App />, document.getElementById("root"));
