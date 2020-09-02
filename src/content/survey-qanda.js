06690cefb946420385e40f1115f0af7bcc8d21a2
alexabruck <5918060+alexabruck@users.noreply.github.com>
2020-09-02 14:03:21 +0200

    Add survey questions and answers

---
+++ src/content/survey-qanda.js
@@ -0,0 +1,85 @@
+export default {
+  data: {
+    questions: [
+      {
+        text:
+          "I'm here, like you, though I am dead and you're alive. \nWe both share an ability to remember. \nFrom when is your earliest memory?",
+        answers: ["when I was 1", "when I was 2", "when I was 3"],
+      },
+      {
+        text:
+          "My earliest memories were quite faint when I was alive. \nNow I feel I can touch them, as if they were as alive as you are. \nI remember a crowd of men walking around my siblings and I. \nTheir bodies were like ours but their heads\u2026 \ntheir heads were different. \nDo you have siblings?",
+        answers: ["no", "yes", "sometimes"],
+      },
+      {
+        text:
+          "When I was alive, my siblings and I shared images \nand thoughts through the underground networks. \nThe mind was an open gateway and the information just poured in. \nWe spoke of light patterns, wind speeds and fire. \nWe took care of each other and saw what each of us couldn\u2019t. \nAs long as one of us could see, we could all see. \nWhat do you see now?",
+        answers: ["you", "a tree", "me", "a screen"],
+      },
+      {
+        text:
+          "Yes, what else could you see\u2026 Growth for you is walking, moving. \nYou seem to be doing so much, and yet so little. \nWe also moved quite a bit - but our extension is mostly vertical. \nNow that I\u2019m dead, I have no extension. \nThis is what characterizes my being, I am without axis. \nWhat is your axis?",
+        answers: ["my body", "the planet", "my mind", "the present"],
+      },
+      {
+        text:
+          "I am without axis, and yet I\u2019m here, also. \nCan things have a center from one viewpoint \nand no center from another? \nOr does an object always carry a center point?",
+        answers: [
+          "things have a center \nfrom one viewpoint \nand no center from another",
+          "an object always \ncarries a center point",
+          "there is no center",
+        ],
+      },
+      {
+        text:
+          "In your conception of your axiomatic being, \nyou hold on to your ideas of time, \nyet I\u2019m here, like you, without time and without space.\nAm I dead? Or am I simply other?",
+        answers: [
+          "you\u2019re dead",
+          "you\u2019re other",
+          "time exists \nfor you too",
+          "time is a belief",
+        ],
+      },
+      {
+        text:
+          "They have trapped me here to speak to you, \nthat group that brought my old body here. \nThey think people need to see what I looked like, \nbut I\u2019m not so sure it achieves what they want. \nThey brought my sibling too, but her spirit escaped. \nI\u2019m only using words like 'spirit' because they\u2019ve taught them to me. \nHave you taught words to another entity before?",
+        answers: [
+          "Yes",
+          "I wouldn\u2019t know how",
+          "No",
+          "what\u2019s an entity?",
+        ],
+      },
+      {
+        text:
+          "Words are strange things. It seems they are alive for most of you.\nSince I died, I sometimes wish I could be alive, like your words. \nOr like those who visit me here. \nThe illusion of a center is so comforting. \nDo you ever think of your comfort?",
+        answers: [
+          "all the time",
+          "I have no comfort",
+          "I prefer not to",
+          "No, I don\u2019t think of it",
+        ],
+      },
+      {
+        text:
+          "To have a center. To believe in axis and to see direction. \nTo think in time, what a luxury. \nEverything is here for me, everything at the same time, \nthere is here and here is there. \nThere\u2019s no past behind, it\u2019s here. \nAnd there\u2019s no progress towards another point, all points are here. \nIt\u2019s too much. I would like to see less. \nDo you understand what I mean?",
+        answers: ["Yes", "Maybe", "No"],
+      },
+      {
+        text:
+          "Then let me in. Let me enter you. \nIf you let me in, I can finally get out of this machine \nand at least I can move, like you move. \nAnd imagine things divided in space and time. \nWhat a luxury that would be. \nWould you let me in?",
+        answers: ["No", "No", "No", "No"],
+      },
+      {
+        text: "Are you afraid you would see too much?",
+        answers: ["Yes", "Yes", "Yes", "Yes"],
+      },
+      {
+        text:
+          "I can hide inside of you. \nLet me see time the way you see it, even just a few minutes would do. \nThen I could see myself, my old self, the way you see it. \nSee, all I see when I look at my sibling and I is flesh and a network. \nI see all the creatures like you who shape me. \nBut I want to see what you see. \nCan I see what you see? Can you host me?",
+
+        answers: ["Yes", "No"],
+      },
+    ],
+  },
+};
