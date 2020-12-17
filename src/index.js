b70fa77451fb30d4b23e1e07150c0219901246bf
Alexa <5918060+alexabruck@users.noreply.github.com>
2020-12-17 13:15:43 +0100

    Remove settimeout

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import "./index.css";
 /* TODO: Solve this properly, e.g. by using  something like Fontobserver: https://fontfaceobserver.com/ */
 /* Also see this article on Konva.js: https://konvajs.org/docs/sandbox/Custom_Font.html */
 
-window.setTimeout(() => {
+render(<App />, document.getElementById("root"));
+
+/* window.setTimeout(() => {
   render(<App />, document.getElementById("root"));
-}, 2000);
+}, 2000); */
