d36100e1c8c07d24e40362fb90ff547af74ea5fc
Alexa <5918060+alexabruck@users.noreply.github.com>
2021-02-04 19:23:06 +0100

    Show warning when landscape mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import * as screenOrientation from "screen-orientation-js";
 import Entity from "./components/Entity";
 import GameController from "./components/GameController";
 
@@ -14,13 +15,22 @@ let store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const App = () => (
-  <Provider store={store}>
-    {isDebugMode && <GameController />}
-    <div style={{backgroundColor: settings.CANVAS_BG_COLOR}}>
-      <Entity />
-    </div>
-  </Provider>
-);
+class App extends React.Component {
+  componentDidMount() {
+    screenOrientation.init({ color: "#fff", bgColor: "#000", fontSize: 3 });
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        {isDebugMode && <GameController />}
+        <div style={{ backgroundColor: settings.CANVAS_BG_COLOR }}>
+          <Entity />
+        </div>
+      </Provider>
+    );
+  }
+}
 
 export default App;
+
+//I want to check the orientation of the device on start, and if its landscape show message
