98bd90d856d10a7a842d56852523fc8a71f05ab0
jelkro <j.kroese@fgga.leidenuniv.nl>
2021-02-05 22:46:09 +0100

    Implemented 2 audio file types for best browser support.

--- src/sounds/SoundMaker.js
+++ src/sounds/SoundMaker.js
@@ -1,32 +1,49 @@
-import soundAnswer1 from "./answer1.flac";
-import soundAnswer2 from "./answer2.flac";
-import soundAnswer3 from "./answer3.flac";
-import soundAnswer4 from "./answer4.flac";
-import soundBackground from "./background.flac";
-import soundPointer from "./pointer.flac";
+//Using both webm and mp3 audioformats, since the combination of these gives highest browser support
+//with lowest filesize (See: https://github.com/goldfire/howler.js#format-recommendations). 
+//Also webm can do gapless looping.
+
+import soundAnswer1Webm from "./answer1.webm";
+import soundAnswer2Webm from "./answer2.webm";
+import soundAnswer3Webm from "./answer3.webm";
+import soundAnswer4Webm from "./answer4.webm";
+import soundAnswer1Mp3 from "./answer1.mp3";
+import soundAnswer2Mp3 from "./answer2.mp3";
+import soundAnswer3Mp3 from "./answer3.mp3";
+import soundAnswer4Mp3 from "./answer4.mp3";
+import soundBackgroundWebm from "./background.webm";
+import soundPointerWebm from "./pointer.webm";
+import soundBackgroundMp3 from "./background.mp3";
+import soundPointerMp3 from "./pointer.mp3";
 
 import {Howl, Howler} from 'howler'; 
 
-const answerSounds = [soundAnswer1, soundAnswer2, soundAnswer3, soundAnswer4];
+const answerSounds = [
+  [soundAnswer1Webm, soundAnswer1Mp3], 
+  [soundAnswer2Webm, soundAnswer2Mp3], 
+  [soundAnswer3Webm, soundAnswer3Mp3], 
+  [soundAnswer4Webm, soundAnswer4Mp3]
+];
 const volumePointer = 0.15;
 const fadeDuration = 300;
 
 class SoundMaker {
   constructor() {
     this.pointerSoundAudio = new Howl({
-      src: soundPointer,
+      src: [soundPointerWebm, soundPointerMp3],
       loop: true,
       volume: volumePointer
     })
     this.backgroundSound = new Howl({
-      src: soundBackground,
+      src: [soundBackgroundWebm, soundBackgroundMp3],
       loop: true,
     })
-    // this.answerSounds = answerSounds.map((sound) => new Audio(sound));
+    this.answerSounds = answerSounds.map((sound) => new Howl({
+      src: sound
+    }));
   }
   playAnswerSound(answerID) {
-    // const audio = this.answerSounds[answerID % this.answerSounds.length];
-    // audio.play();
+    const sound = this.answerSounds[answerID % this.answerSounds.length];
+    sound.play();
   }
   playPointerSound() {
     this.pointerSoundAudio.play();
