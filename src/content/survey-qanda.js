55cf2c72c1b9d5df83dfb5d1d25b57912a47aa1f
jelkro <jelgerkroese@gmail.com>
2022-03-02 19:14:30 +0100

    Updates line breaks so all texts fit on mobile screen.

--- src/content/survey-qanda.js
+++ src/content/survey-qanda.js
@@ -3,76 +3,76 @@ export default {
     questions: [
       {
         text:
-          "I'm here, like you, though I am dead and you're alive. \nWe both share an ability to remember. \nFrom when is your earliest memory?",
+          "I'm here, like you,\nthough I am dead and you're alive.\nWe both share an ability to remember.\nFrom when is your earliest memory?",
         answers: [
-          "when I was 1",
-          "when I was 2",
-          "when I was 3",
-          "when I was 5",
+          "when I\nwas 1", //falls of screen
+          "when I\nwas 2",
+          "when I\nwas 3",
+          "when I\nwas 5",
         ],
       },
       {
         text:
-          "My earliest memories were quite faint when I was alive. \nNow I feel I can touch them, as if they were as alive as you are. \nI remember a crowd of men walking around my siblings and I. \nTheir bodies were like ours but their heads\u2026 \ntheir heads were different. \nDo you have siblings?",
+          "My earliest memories were quite faint \nwhen I was alive. \nNow I feel I can touch them, \nas if they were as alive as you are. \nI remember a crowd of men \nwalking around my siblings and I. \nTheir bodies were like ours but their heads\u2026 \ntheir heads were different. \nDo you have siblings?",
         answers: ["no", "yes", "sometimes"],
       },
       {
         text:
-          "When I was alive, my siblings and I shared images \nand thoughts through the underground networks. \nThe mind was an open gateway and the information just poured in. \nWe spoke of light patterns, wind speeds and fire. \nWe took care of each other and saw what each of us couldn\u2019t. \nAs long as one of us could see, we could all see. \nWhat do you see now?",
+          "When I was alive, my siblings and I shared images\nand thoughts through the underground networks.\nThe mind was an open gateway\nand the information just poured in.\nWe spoke of light patterns, wind speeds and fire.\nWe took care of each other\nand saw what each of us couldn\u2019t.\nAs long as one of us could see, we could all see.\nWhat do you see now?",
         answers: ["you", "a tree", "me", "a screen"],
       },
       {
         text:
-          "Yes, what else could you see\u2026 Growth for you is walking, moving. \nYou seem to be doing so much, and yet so little. \nWe also moved quite a bit - but our extension is mostly vertical. \nNow that I\u2019m dead, I have no extension. \nThis is what characterizes my being, I am without axis. \nWhat is your axis?",
+          "Yes, what else could you see\u2026\nGrowth for you is walking, moving.\nYou seem to be doing so much, and yet so little.\nWe also moved quite a bit\nbut our extension is mostly vertical.\nNow that I\u2019m dead, I have no extension.\nThis is what characterizes my being, I am without axis.\nWhat is your axis?",
         answers: ["my body", "the planet", "my mind", "the present"],
       },
       {
         text:
-          "I am without axis, and yet I\u2019m here, also. \nCan things have a center from one viewpoint \nand no center from another? \nOr does an object always carry a center point?",
+          "I am without axis, and yet I\u2019m here, also.\nCan things have a center from one viewpoint\nand no center from another?\nOr does an object always carry a center point?",
         answers: [
-          "things have a center \nfrom one viewpoint \nand no center from another",
-          "an object always \ncarries a center point",
-          "there is no center",
+          "there is\nno center",
+          "things have a center\nfrom one viewpoint\nand no center from another",
+          "an object\nalways carries\na center point",
         ],
       },
       {
         text:
-          "In your conception of your axiomatic being, \nyou hold on to your ideas of time, \nyet I\u2019m here, like you, without time and without space.\nAm I dead? Or am I simply other?",
+          "In your conception of your axiomatic being,\nyou hold on to your ideas of time,\nyet I\u2019m here, like you,\nwithout time and without space.\nAm I dead? Or am I simply other?",
         answers: [
-          "you\u2019re dead",
-          "you\u2019re other",
-          "time exists \nfor you too",
-          "time is a belief",
+          "you\u2019re\ndead",
+          "you\u2019re\nother",
+          "time exists\nfor you too",
+          "time is\na belief",
         ],
       },
       {
         text:
-          "They have trapped me here to speak to you, \nthat group that brought my old body here. \nThey think people need to see what I looked like, \nbut I\u2019m not so sure it achieves what they want. \nThey brought my sibling too, but her spirit escaped. \nI\u2019m only using words like 'spirit' because they\u2019ve taught them to me. \nHave you taught words to another entity before?",
+          "They have trapped me here to speak to you,\nthat group that brought my old body here.\nThey think people need to see what I looked like,\nbut I\u2019m not so sure it achieves what they want.\nThey brought my sibling too, but her spirit escaped.\nI\u2019m only using words like 'spirit'\nbecause they\u2019ve taught them to me.\nHave you taught words to another entity before?",
         answers: [
           "Yes",
-          "I wouldn\u2019t know how",
+          "I wouldn\u2019t\nknow how",
           "No",
-          "what\u2019s an entity?",
+          "what\u2019s\nan entity?",
         ],
       },
       {
         text:
-          "Words are strange things. It seems they are alive for most of you.\nSince I died, I sometimes wish I could be alive, like your words. \nOr like those who visit me here. \nThe illusion of a center is so comforting. \nDo you ever think of your comfort?",
+          "Words are strange things.\nIt seems they are alive for most of you.\nSince I died, I sometimes wish I could be alive,\nlike your words.\nOr like those who visit me here.\nThe illusion of a center is so comforting.\nDo you ever think of your comfort?",
         answers: [
-          "all the time",
-          "I have no comfort",
-          "I prefer not to",
-          "No, I don\u2019t think of it",
+          "all\nthe time",
+          "I have\nno comfort",
+          "I prefer\nnot to",
+          "No,\nI don\u2019t",
         ],
       },
       {
         text:
-          "To have a center. To believe in axis and to see direction. \nTo think in time, what a luxury. \nEverything is here for me, everything at the same time, \nthere is here and here is there. \nThere\u2019s no past behind, it\u2019s here. \nAnd there\u2019s no progress towards another point, all points are here. \nIt\u2019s too much. I would like to see less. \nDo you understand what I mean?",
+          "To have a center.\nTo believe in axis and to see direction.\nTo think in time, what a luxury.\nEverything is here for me, everything at the same time,\nthere is here and here is there.\nThere\u2019s no past behind, it\u2019s here.\nAnd there\u2019s no progress towards another point,\nall points are here.\nIt\u2019s too much. I would like to see less.\nDo you understand what I mean?",
         answers: ["Yes", "Maybe", "No"],
       },
       {
         text:
-          "Then let me in. Let me enter you. \nIf you let me in, I can finally get out of this machine \nand at least I can move, like you move. \nAnd imagine things divided in space and time. \nWhat a luxury that would be. \nWould you let me in?",
+          "Then let me in. Let me enter you.\nIf you let me in,\nI can finally get out of this machine\nand at least I can move, like you move.\nAnd imagine things divided in space and time.\nWhat a luxury that would be.\nWould you let me in?",
         answers: ["No", "No", "No", "No"],
       },
       {
@@ -81,8 +81,7 @@ export default {
       },
       {
         text:
-          "I can hide inside of you. \nLet me see time the way you see it, even just a few minutes would do. \nThen I could see myself, my old self, the way you see it. \nSee, all I see when I look at my sibling and I is flesh and a network. \nI see all the creatures like you who shape me. \nBut I want to see what you see. \nCan I see what you see? Can you host me?",
-
+          "I can hide inside of you.\nLet me see time the way you see it,\neven just a few minutes would do.\nThen I could see myself,\nmy old self, the way you see it.\nSee, all I see when I look at my sibling and I\nis flesh and a network.\nI see all the creatures like you who shape me.\nBut I want to see what you see.\nCan I see what you see? Can you host me?",
         answers: ["Yes", "No"],
       },
     ],
